564a07ef14b5e3e8147e9c695eb759ee1d50806f
Naman Singhal <namansinghal0720@gmail.com>
2026-08-19 01:56:38 +0530

    Fix CORS for Vercel frontend

--- ecom-proj/src/main/java/com/naman/ecom_proj/security/SecurityConfig.java
+++ ecom-proj/src/main/java/com/naman/ecom_proj/security/SecurityConfig.java
@@ -75,9 +75,18 @@ public class SecurityConfig {
 
         CorsConfiguration configuration = new CorsConfiguration();
 
-        configuration.setAllowedOrigins(List.of("http://localhost:5173"));
+        configuration.setAllowedOrigins(List.of(
+                "http://localhost:5173",
+                "https://ecommerce-fullstack-naman-5752.vercel.app"
+        ));
 
-        configuration.setAllowedMethods(List.of("*"));
+        configuration.setAllowedMethods(List.of(
+                "GET",
+                "POST",
+                "PUT",
+                "DELETE",
+                "OPTIONS"
+        ));
 
         configuration.setAllowedHeaders(List.of("*"));
 
